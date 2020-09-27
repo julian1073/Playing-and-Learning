8d4f90ff495087877c60abf5351219bc641a4ad2
Erika <ejuliethmoreno@gmail.com>
2020-09-26 19:48:54 -0500

    Modulos

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -7,11 +7,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvRegistrar;
     TextView tvRecuperarClave;
+    Button BModulos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         //Creamos variable y le asociamos tvRegistrar del activity.main
         tvRegistrar = (TextView) findViewById(R.id.tvRegistrar);
         tvRecuperarClave = (TextView) findViewById(R.id.tvRecuperarClave);
+        BModulos = (Button) findViewById(R.id.BModulos);
+
 
         tvRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intentRec = new Intent(MainActivity.this, RecuperarClave.class);
+                MainActivity.this.startActivity(intentRec);
+            }
+        });
+        BModulos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRec = new Intent(MainActivity.this, Modulos.class);
                 MainActivity.this.startActivity(intentRec);
             }
         });
