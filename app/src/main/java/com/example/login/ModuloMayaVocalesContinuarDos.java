df77a18581ec076784176ed89a649e3104827b54
Sandra <sandramilenamoreno@ucundinamarca.edu.co>
2020-10-07 18:17:39 -0500

    VocalesVideo

--- app/src/main/java/com/example/login/ModuloMayaVocalesContinuarDos.java
+++ app/src/main/java/com/example/login/ModuloMayaVocalesContinuarDos.java
@@ -1,16 +1,19 @@
 package com.example.login;
 
-import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ModuloMayaVocalesContinuarDos extends AppCompatActivity {
     MediaPlayer uvas;
     ImageButton botonUva;
+    VideoView videoVocales;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,7 @@ public class ModuloMayaVocalesContinuarDos extends AppCompatActivity {
 
         botonUva = (ImageButton)findViewById(R.id.botonUva);
         uvas = MediaPlayer.create(this, R.raw.uva);
+        videoVocales = findViewById(R.id.videoVocales);
 
         botonUva.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +30,14 @@ public class ModuloMayaVocalesContinuarDos extends AppCompatActivity {
                 uvas.start();
             }
         });
+
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.videovocales;
+        Uri uri = Uri.parse(videoPath);
+        videoVocales.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoVocales.setMediaController(mediaController);
+        mediaController.setAnchorView(videoVocales);
+
     }
 }
