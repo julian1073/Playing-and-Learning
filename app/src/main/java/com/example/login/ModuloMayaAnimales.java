cf472556122a5271ba200da2be0dac0af37ffdcb
Tania Martinez <tmmartinez@ucundinamarca.edu.co>
2020-10-09 10:49:02 -0500

    ModuloMayaCategoriaAnimales

--- app/src/main/java/com/example/login/ModuloMayaAnimales.java
+++ app/src/main/java/com/example/login/ModuloMayaAnimales.java
@@ -3,16 +3,29 @@ package com.example.login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ModuloMayaAnimales extends AppCompatActivity {
+public class
+ModuloMayaAnimales extends AppCompatActivity {
+
+    ImageButton siguiente;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.modulo_maya_animales);
+
+        siguiente = (ImageButton) findViewById(R.id.siguiente);
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRec = new Intent(ModuloMayaAnimales.this, ModuloMayaAnimalesUno.class);
+                ModuloMayaAnimales.this.startActivity(intentRec);
+            }
+        });
+
     }
 }
