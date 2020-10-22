b4c4fd307d0d7e1757543bde2419a5464678388f
Sandra <sandramilenamoreno@ucundinamarca.edu.co>
2020-10-22 16:05:26 -0500

    Anexo

--- app/src/main/java/com/example/login/ModuloSuperBoltVocalesIngles.java
+++ app/src/main/java/com/example/login/ModuloSuperBoltVocalesIngles.java
@@ -2,13 +2,18 @@ package com.example.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ModuloSuperBoltVocalesIngles extends AppCompatActivity {
     ImageButton siguienteVocalesIngles;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +30,49 @@ public class ModuloSuperBoltVocalesIngles extends AppCompatActivity {
             }
         });
 
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.activity_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.itModuloMaya) {
+
+            startActivity(new Intent(ModuloSuperBoltVocalesIngles.this, ModuloMaya.class));
+
+            return true;
+        } else if (id == R.id.itModuloBolt) {
+
+            startActivity(new Intent(ModuloSuperBoltVocalesIngles.this, ModuloBolt.class));
+
+            return true;
+        } else if (id == R.id.itModuloSuperBolt) {
+
+            startActivity(new Intent(ModuloSuperBoltVocalesIngles.this, ModuloSuperBolt.class));
+
+            return true;
+        } else if (id == R.id.itInicio) {
+
+            startActivity(new Intent(ModuloSuperBoltVocalesIngles.this, Modulos.class));
+
+            return true;
+        } else if (id == R.id.itCerrarSesion) {
+
+            mAuth.signOut();
+            startActivity(new Intent(ModuloSuperBoltVocalesIngles.this, MainActivity.class));
+            finish();
+
+            return true;
+        } else if (id == R.id.itMiCuenta) {
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
